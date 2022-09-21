d52c08a5af2fae0ba6096e510d8741b312e570a0
dexter22245 <113989729+dexter22245@users.noreply.github.com>
2022-09-21 15:42:42 +0530

    Update update.py

--- update.py
+++ update.py
@@ -37,7 +37,7 @@ try:
     if len(UPSTREAM_REPO) == 0:
        raise TypeError
 except:
-    UPSTREAM_REPO = "https://github.com/weebzone/WZML"
+    UPSTREAM_REPO = "https://github.com/alokhj83077/wzmlmain"
 try:
     if len(UPSTREAM_BRANCH) == 0:
        raise TypeError
