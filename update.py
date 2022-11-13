ddbde86fa451c13aaedeaec1fdc3c4d2f9847420
dexter22245 <113989729+dexter22245@users.noreply.github.com>
2022-11-13 18:07:17 +0530

    Update update.py

--- update.py
+++ update.py
@@ -37,12 +37,12 @@ try:
     if len(UPSTREAM_REPO) == 0:
        raise TypeError
 except:
-    UPSTREAM_REPO = "https://github.com/alokhj83077/wzmlmain"
+    UPSTREAM_REPO = "https://github.com/dexter1834/WZML"
 try:
     if len(UPSTREAM_BRANCH) == 0:
        raise TypeError
 except:
-    UPSTREAM_BRANCH = 'update'
+    UPSTREAM_BRANCH = 'master'
 
 if ospath.exists('.git'):
     srun(["rm", "-rf", ".git"])
